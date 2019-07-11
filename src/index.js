14dd9f3a7dab44e71af96e64c92c17f25d6651a0
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2019-07-11 19:24:42 +0100

    Initial commit, set up project, add TS

---
+++ src/index.js
@@ -0,0 +1,91 @@
+/**
+ * Export functions you want to work with, see documentation for details:
+ * https://github.com/zeplin/zeplin-extension-documentation
+ */
+
+function layer(context, selectedLayer) {
+    // const containerType = "styleguide" in context ? "styleguide" : "project";
+    // const object = {
+    //     layerName: selectedLayer.name,
+    //     [`${containerType}Name`]: context[containerType].name,
+    //     // context: context
+    // };
+    //
+    // const JSONString = JSON.stringify(object, null, 2);
+    //
+    // return {
+    //     code: JSONString,
+    //     language: "json"
+    // };
+
+    console.log({selectedLayer});
+
+    return `const ${selectedLayer.name} = styled.div
+    fills: ${JSON.stringify(selectedLayer.fills, null, 2)}
+    `;
+}
+
+function screen(context, selectedVersion, selectedScreen) {
+
+}
+
+function component(context, selectedVersion, selectedComponent) {
+    console.log(selectedComponent);
+}
+
+function colors(context) {
+
+}
+
+function textStyles(context) {
+
+}
+
+function exportColors(context) {
+
+}
+
+function exportTextStyles(context) {
+
+}
+
+/**
+ * The following functions will be deprecated. Your extensions can export them to support old versions of Zeplin's macOS app.
+ * See Zeplin Extensions migration guide for details:
+ * https://zpl.io/shared-styleguides-extensions-migration-guide
+ */
+
+function styleguideColors(context, colors) {
+
+}
+
+function styleguideTextStyles(context, textStyles) {
+
+}
+
+function exportStyleguideColors(context, colors) {
+
+}
+
+function exportStyleguideTextStyles(context, textStyles) {
+
+}
+
+function comment(context, text) {
+
+}
+
+export default {
+    layer,
+    screen,
+    component,
+    colors,
+    textStyles,
+    exportColors,
+    exportTextStyles,
+    styleguideColors,
+    styleguideTextStyles,
+    exportStyleguideColors,
+    exportStyleguideTextStyles,
+    comment
+};
---
+++ src/utils/nameParser.ts
@@ -0,0 +1,4 @@
+function nameParser(name: string): string {
+  // should capitalize first letter of each word
+  // should remove whitespaces
+}
